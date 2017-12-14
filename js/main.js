dd43237b87bf736602ca6decde6ee23ae272452d
Alexandr Derkach <easysholder@gmail.com>
2017-12-14 20:09:43 +0200

    123

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
 $(document).ready(function () {
-    $('#top-lang-panel').on('click', firstButtonHandler);
-    $('#drop-lang-panel').on('click', secondButtonHandler);
+	$('#top-lang-panel').on('click', firstButtonHandler);
+	$('#drop-lang-panel').on('click', secondButtonHandler);
 
-    function secondButtonHandler() {
-        $('#drop-lang-panel').removeClass();
-        $('#drop-lang-panel').addClass('top-panel-lang-panel-container')
-        $('#top-lang-panel').removeClass();
-        $('#top-lang-panel').addClass('drop-lang-cont');
-        $('span#drop-lang-panel-inner').css('color', '#009a49');
-        $('span#top-lang-panel-inner').css('color', '#ffffff');
-    }
+	function secondButtonHandler() {
+		$('#drop-lang-panel').removeClass();
+		$('#drop-lang-panel').addClass('top-panel-lang-panel-container')
+		$('#top-lang-panel').removeClass();
+		$('#top-lang-panel').addClass('drop-lang-cont');
+		$('span#drop-lang-panel-inner').css('color', '#009a49');
+		$('span#top-lang-panel-inner').css('color', '#ffffff');
+	}
 
-    function firstButtonHandler() {
-        $('#drop-lang-panel').removeClass();
-        $('#drop-lang-panel').addClass('drop-lang-cont')
-        $('#top-lang-panel').removeClass();
-        $('#top-lang-panel').addClass('top-panel-lang-panel-container');
-        $('span#drop-lang-panel-inner').css('color', '#ffffff');
-        $('span#top-lang-panel-inner').css('color', '#009a49');
-    }
+	function firstButtonHandler() {
+		$('#drop-lang-panel').removeClass();
+		$('#drop-lang-panel').addClass('drop-lang-cont')
+		$('#top-lang-panel').removeClass();
+		$('#top-lang-panel').addClass('top-panel-lang-panel-container');
+		$('span#drop-lang-panel-inner').css('color', '#ffffff');
+		$('span#top-lang-panel-inner').css('color', '#009a49');
+	}
+	$('#goods-list').on('click', function () {
+		$('#the-list').slideToggle(0, function () {
+			$('#goodslistarrow').toggleClass('goods-list-arrow-rotate');
+			$(this).toggleClass('expand').css('display', '');
+		})
+	})
+	$(function () {
+		$("#phone-inp").mask("+38 (999) 999-9999");
+	});
 });
-$(document).ready(function () {
-    $('#goods-list').on('click', function () {
-        $('#the-list').slideToggle(0, function () {
-            $('#goodslistarrow').toggleClass('goods-list-arrow-rotate');
-            $(this).toggleClass('expand').css('display', '');
-        })
-    })
-});
-
-
